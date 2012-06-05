1c1e231bd6f7f49e6f71a01cd6781295dc74de7e
Christopher Klapp <christopher@klapp.name>
2012-06-05 18:52:50 -0400

    raspberrypi extend device

--- lib/gpio/device.rb
+++ lib/gpio/device.rb
@@ -1,17 +1,17 @@
 module GPIO
 	module Device
-		def self.software_pin(pin)
-			raise "That software pin doesn't exist." unless software.include? pin
-			mapping == :software ? pin : software[hardware.index(pin)]
+		def software_pin(pin)
+			raise "That software pin doesn't exist." unless software_pins.include? pin
+			mapping == :software ? pin : software_pins[hardware_pins.index(pin)]
 		end
-		def self.hardware_pin(pin)
-			raise "That hardware pin doesn't exist." unless hardware.include? pin
-			mapping == :hardware ? pin : hardware[software.index(pin)]
+		def hardware_pin(pin)
+			raise "That hardware pin doesn't exist." unless hardware_pins.include? pin
+			mapping == :hardware ? pin : hardware_pins[software_pins.index(pin)]
 		end
-		def self.load_pins
+		def load_pins
 			get_pins(:hardware).map{|pin| Pin.new(pin,get_direction(pin),self)}
 		end
-		def self.unexport_all!
+		def unexport_all!
 			load_pins.map(&unexport!)
 		end
 	end
--- lib/gpio/raspberry_pi.rb
+++ lib/gpio/raspberry_pi.rb
@@ -1,5 +1,7 @@
 module GPIO
 	module RaspberryPi
+		extend Device
+
 		def self.mapping
 			:hardware
 		end
@@ -42,21 +44,6 @@ module GPIO
 		def self.write(software_pin,value)
 			raise "This pin is an input." if get_direction(software_pin) == 'in'
 			`sudo bash -c "echo #{value} > /sys/class/gpio/gpio#{software_pin}/value && echo true || false"`.chomp == 'true'
-		end
-
-		def self.software_pin(pin)
-			raise "That software pin doesn't exist." unless software_pins.include? pin
-			mapping == :software ? pin : software_pins[hardware_pins.index(pin)]
-		end
-		def self.hardware_pin(pin)
-			raise "That hardware pin doesn't exist." unless hardware_pins.include? pin
-			mapping == :hardware ? pin : hardware_pins[software_pins.index(pin)]
-		end
-		def self.load_pins
-			get_pins(:hardware).map{|pin| Pin.new(pin,get_direction(pin),self)}
-		end
-		def self.unexport_all!
-			load_pins.map(&unexport!)
 		end
 	end
 end

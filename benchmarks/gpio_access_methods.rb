0daa4e48049f969d5f01fee778bb3e11775e7cd4
Christopher Klapp <christopher@klapp.name>
2012-06-07 11:36:26 -0300

    benchmarks: added timings

--- benchmarks/gpio_access_methods.rb
+++ benchmarks/gpio_access_methods.rb
@@ -15,3 +15,18 @@ Benchmark.bm do |x|
 	puts "io.binread"
 	x.report { n.times do IO.binread(file) end }
 end
+
+=begin
+#run on Raspberry Pi
+       user     system      total        real
+shelling out
+   0.070000   0.180000   5.250000 (  8.334373)
+io.read
+   0.010000   0.010000   0.020000 (  0.021178)
+io.read.chomp!
+   0.020000   0.000000   0.020000 (  0.021982)
+io.read(file, 1)
+   0.010000   0.010000   0.020000 (  0.015710)
+io.binread
+   0.030000   0.000000   0.030000 (  0.025154)
+=end

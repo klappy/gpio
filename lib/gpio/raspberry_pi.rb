eac9288945bb613c101ae3d4542b00732d7f6807
Christopher Klapp <christopher@klapp.name>
2012-06-07 12:08:23 -0300

    raspberry_pi.rb: extracted paths to separate methods to allow for generic gpio methods to be moved to device and allow raspberr_pi.rb and other modules that extend device to be configuration.

--- lib/gpio/raspberry_pi.rb
+++ lib/gpio/raspberry_pi.rb
@@ -11,13 +11,32 @@ module GPIO
 		def self.software_pins
 			[0,1,4,14,15,17,18,21,22,23,24,10,9,25,11,8,7]
 		end
+		
+		def self.base_path
+			'/sys/class/gpio/'
+		end
+		def self.pin_path(n)
+			"#{base_path}gpio#{n}/"
+		end
+		def self.direction_path(n)
+			"#{pin_path(n)}direction"
+		end
+		def self.value_path(n)
+			"#{pin_path(n)}value"
+		end
+		def self.export_path
+			"#{base_path}export"
+		end
+		def self.unexport_path
+			"#{base_path}unexport"
+		end
 
 		def self.get_pins(mapping=@mapping)
-			pins = `sudo ls /sys/class/gpio`.scan(/(?:gpio)(\d+)/).flatten.map!(&:to_i)
+			pins = `sudo ls #{base_path}`.scan(/(?:gpio)(\d+)/).flatten.map!(&:to_i)
 			pins.map!{|pin| Pin.new(pin,nil,self).pin}
 		end
 		def self.get_direction(software_pin)
-			`sudo cat /sys/class/gpio/gpio#{software_pin}/direction`.chomp
+			`sudo cat #{direction_path(software_pin)}`.chomp
 		end
 
 		def self.initialize_pin(software_pin, direction)
@@ -26,24 +45,24 @@ module GPIO
 		end
 
 		def self.exported?(software_pin)
-			`sudo [ -d /sys/class/gpio/gpio#{software_pin} ] && echo true || false`.chomp == 'true'
+			`sudo [ -d #{pin_path(software_pin)} ] && echo true || false`.chomp == 'true'
 		end
 		def self.export!(software_pin,direction)
-			`sudo bash -c "echo #{software_pin} > /sys/class/gpio/export"`
-			`sudo bash -c "echo #{direction} > /sys/class/gpio/gpio#{software_pin}/direction"`
+			`sudo bash -c "echo #{software_pin} > #{export_path}"`
+			`sudo bash -c "echo #{direction} > #{direction_path(software_pin)}"`
 			exported?(software_pin)
 		end
 		def self.unexport!(software_pin)
-			`sudo bash -c "echo #{software_pin} > /sys/class/gpio/unexport"`
+			`sudo bash -c "echo #{software_pin} > #{unexport_path}"`
 			!exported?(software_pin)
 		end
 
 		def self.read(software_pin)
-			`sudo cat /sys/class/gpio/gpio#{software_pin}/value`.chomp == '1'
+			`sudo cat #{value_path(software_pin)}`.chomp == '1'
 		end
 		def self.write(software_pin,value)
 			raise "This pin is an input." if get_direction(software_pin) == 'in'
-			`sudo bash -c "echo #{value} > /sys/class/gpio/gpio#{software_pin}/value && echo true || false"`.chomp == 'true'
+			`sudo bash -c "echo #{value} > #{value_path(software_pin)} && echo true || false"`.chomp == 'true'
 		end
 	end
 end

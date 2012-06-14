5eee4a1041b8c7357da86f2833025e3222569873
Christopher Klapp <christopher@klapp.name>
2012-06-13 21:34:17 -0400

    gpio.rb: updated to include new outputs

--- lib/gpio.rb
+++ lib/gpio.rb
@@ -1,1 +1,1 @@
-%w[device devices/raspberry_pi pin pins/input_pin pins/output_pin sensor sensors/motion_detector sensors/ping_sonar].each{|file| require File.dirname(__FILE__)+'/gpio/'+file}
+%w[device devices/raspberry_pi pin pins/input_pin pins/output_pin sensor sensors/motion_detector sensors/ping_sonar outputs/led outputs/traffic_light].each{|file| require File.dirname(__FILE__)+'/gpio/'+file}

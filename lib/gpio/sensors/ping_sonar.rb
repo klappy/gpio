38a197fdc4f61289ba7a902bc5e664e55e598fc0
Christopher Klapp <christopher@klapp.name>
2012-06-14 08:57:38 -0400

    ping_sonar.rb: added detect_time and prep_time for looking into timings

--- lib/gpio/sensors/ping_sonar.rb
+++ lib/gpio/sensors/ping_sonar.rb
@@ -1,16 +1,18 @@
 module GPIO
 	class PingSonar < Sensor
-		attr_reader :output
+		attr_reader :output, :detect_time, :prep_time
 		def initialize(params)
 			@output = OutputPin.new params
 			super(params)
 		end
 		def detect
+			begin_time = Time.now.to_f
 			@readings = []
 			@output.on;
 			@output.off;
+			@prep_time = Time.now.to_f - begin_time
 			start = Time.now.to_f
-			until Time.now.to_f - start > 0.001
+			until Time.now.to_f - start > 0.1
 				@readings.push @input.read
 			end
 			@detect_time = Time.now.to_f - start

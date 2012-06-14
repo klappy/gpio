3069417fd7c3bba47830f884ea9f5ca5af98efce
Christopher Klapp <christopher@klapp.name>
2012-06-14 09:02:56 -0400

    traffic_light.rb: moved missing end to proper place

--- lib/gpio/outputs/traffic_light.rb
+++ lib/gpio/outputs/traffic_light.rb
@@ -33,33 +33,33 @@ module GPIO
 			when :warn; stop
 			when :stop; go
 			end
-			def cycle(iterations=1, durations={})
-				iterations.times do
-					go and sleep durations[:go] || 3
-					warn and sleep durations[:warn] || 1
-					stop and sleep durations[:stop] || 2
-				end
+		end
+		def cycle(iterations=1, durations={})
+			iterations.times do
+				go and sleep durations[:go] || 3
+				warn and sleep durations[:warn] || 1
+				stop and sleep durations[:stop] || 2
 			end
+		end
 
-			def power_outage
-				red.off
-				yellow.off
-				green.off
-				@state = :power_outage
-			end
-			def all(state=:off)
-				red.send(state)
-				yellow.send(state)
-				green.send(state)
-			end
-			def power_restore(iterations=1)
-				iterations.times do
-					all :on
-					sleep 1
-					all :off
-					sleep 1
-					@state = :power_restore
-				end
+		def power_outage
+			red.off
+			yellow.off
+			green.off
+			@state = :power_outage
+		end
+		def all(state=:off)
+			red.send(state)
+			yellow.send(state)
+			green.send(state)
+		end
+		def power_restore(iterations=1)
+			iterations.times do
+				all :on
+				sleep 1
+				all :off
+				sleep 1
+				@state = :power_restore
 			end
 		end
 	end

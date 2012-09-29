6e8d4684d7d1e237dcc8dc0e4cd501c0ccfcbaa1
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-09-29 17:10:27 +0200

    adding gpio mapping for revision 2 raspberry pi - fixes issue #1.

---
+++ lib/gpio/devices/raspberry_pi_revision_2.rb
@@ -0,0 +1,18 @@
+module GPIO
+  module RaspberryPiRevision2
+    extend Device
+
+    VALIDATE_FILE = "/sys/class/gpio/gpiochip0/label"
+    VALIDATE_VALUE = "bcm2708_gpio"
+
+    MAPPING = :hardware
+    HARDWARE_PINS = [3,5,7,8,10,11,12,13,15,16,17,19,21,22,23,24,26]
+    SOFTWARE_PINS = [2,3,4,14,15,17,18,27,22,23,24,10,9,25,11,8,7]
+    BASE_PATH = '/sys/class/gpio/'
+    EXPORT_PATH = "#{BASE_PATH}export"
+    UNEXPORT_PATH = "#{BASE_PATH}unexport"
+    PIN_PREFIX = "gpio"
+    DIRECTION_FILE = "direction"
+    VALUE_FILE = "value"
+  end
+end
